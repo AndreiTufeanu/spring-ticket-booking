62798c1144a4f12be82f552e5321e1a4406e66e6
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-08-15 23:35:08 +0300

    feat(event): add query for upcoming events

--- backend/src/main/java/com/andreitufeanu/backend/event/repository/EventRepository.java
+++ backend/src/main/java/com/andreitufeanu/backend/event/repository/EventRepository.java
@@ -3,7 +3,12 @@ package com.andreitufeanu.backend.event.repository;
 import com.andreitufeanu.backend.event.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
+
+    public List<Event> findByEventDateGreaterThanOrderByEventDateAsc(Instant currentDate);
+
 }
