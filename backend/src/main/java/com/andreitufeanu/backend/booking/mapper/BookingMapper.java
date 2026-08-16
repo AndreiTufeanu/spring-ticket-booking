2168126290a87d139c51872ece4003b429bb050b
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-08-16 23:43:19 +0300

    feat(booking): add booking mapper

---
+++ backend/src/main/java/com/andreitufeanu/backend/booking/mapper/BookingMapper.java
@@ -0,0 +1,15 @@
+package com.andreitufeanu.backend.booking.mapper;
+
+import com.andreitufeanu.backend.booking.dto.BookingDto;
+import com.andreitufeanu.backend.booking.entity.Booking;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface BookingMapper {
+
+    @Mapping(target = "eventId", source = "event.id")
+    @Mapping(target = "eventTitle", source = "event.title")
+    @Mapping(target = "eventDate", source = "event.eventDate")
+    BookingDto toDto(Booking booking);
+}
