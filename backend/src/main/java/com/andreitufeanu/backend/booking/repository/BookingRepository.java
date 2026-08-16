bb0b9f39e918deda834c78556496b9ae6eef44e3
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-08-16 23:44:17 +0300

    feat(booking): add user booking query

--- backend/src/main/java/com/andreitufeanu/backend/booking/repository/BookingRepository.java
+++ backend/src/main/java/com/andreitufeanu/backend/booking/repository/BookingRepository.java
@@ -2,8 +2,14 @@ package com.andreitufeanu.backend.booking.repository;
 
 import com.andreitufeanu.backend.booking.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BookingRepository extends JpaRepository<Booking, UUID> {
+
+    @Query("SELECT b FROM Booking b WHERE b.user.id = :userId")
+    List<Booking> findBookingsForUser(@Param("userId") UUID userId);
 }
