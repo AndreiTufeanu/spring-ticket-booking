ce14c95f48b5b8f0895fab550da489cb5b2bb513
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-08-16 23:41:53 +0300

    feat(booking): add booking DTOs

---
+++ backend/src/main/java/com/andreitufeanu/backend/booking/dto/BookingDto.java
@@ -0,0 +1,12 @@
+package com.andreitufeanu.backend.booking.dto;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record BookingDto(
+        UUID id,
+        UUID eventId,
+        int seatNumber,
+        String eventTitle,
+        Instant eventDate)
+{ }
