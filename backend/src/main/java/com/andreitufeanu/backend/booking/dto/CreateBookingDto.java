d332c7372efd84cce4494f328e48bbaf31566c67
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-09-04 10:48:09 +0300

    feat(booking): remove seat number from CreateBookingDto

--- backend/src/main/java/com/andreitufeanu/backend/booking/dto/CreateBookingDto.java
+++ backend/src/main/java/com/andreitufeanu/backend/booking/dto/CreateBookingDto.java
@@ -8,9 +8,5 @@ import java.util.UUID;
 
 public record CreateBookingDto(
         @NotNull(message = "Event id is required")
-        UUID eventId,
-
-        @Min(value = 1, message = "Seat number must be at least {value}")
-        @Max(value = 1_000_000, message = "Seat number can't exceed {value}")
-        int seatNumber)
+        UUID eventId)
 { }
